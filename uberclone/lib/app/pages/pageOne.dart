177d18661cb29f17d7a8380d1ebf037250d6bf24
Armano <armanobarrosalvesh@gmail.com>
2022-05-08 16:54:36 -0300

    Arquivos Essenciais

---
+++ uberclone/lib/app/pages/pageOne.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageTest extends StatelessWidget {
+  const PageTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
