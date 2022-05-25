4fca97f79f184654107a4d39aa8ce2322b40ca95
Yuri Nascimento <yurinascimennto@gmail.com>
2022-05-25 18:06:20 -0300

    informação de Ponto-Destino para o firebase

--- uberclone/lib/views/R01/T2/R01-T2.dart
+++ uberclone/lib/views/R01/T2/R01-T2.dart
@@ -7,6 +7,7 @@ class r01_t2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController _whereToPoint = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black12,
@@ -15,23 +16,44 @@ class r01_t2 extends StatelessWidget {
       ),
       backgroundColor: Colors.black,
       body: Center(
-        child: TextFormField(
-          controller: pickuppoint,
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            hintText: "Input",
-            filled: true,
-            fillColor: const Color.fromARGB(255, 207, 205, 205),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
+        child: Column(children: [
+          TextFormField(
+            controller: pickuppoint,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              hintText: "Input",
+              filled: true,
+              fillColor: const Color.fromARGB(255, 207, 205, 205),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            onEditingComplete: () => pickup_point_location(pickuppoint.text),
+            validator: (value) {
+              if (value == null) return "Enter pickup point";
+              return null;
+            },
+          ),
+          Container(
+            color: const Color.fromARGB(255, 20, 20, 20),
+            child: TextFormField(
+              controller: _whereToPoint,
+              style: const TextStyle(color: Colors.white),
+              cursorColor: Colors.white,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                prefixText: '   ',
+                hintText: 'Para onde?',
+                hintStyle: TextStyle(color: Color.fromARGB(255, 209, 209, 209)),
+              ),
+              onEditingComplete: () => whereToLocation(_whereToPoint.text),
+              validator: (value) {
+                if (value == null) return "Enter where to point";
+                return null;
+              },
             ),
           ),
-          onEditingComplete: () => pickup_point_location(pickuppoint.text),
-          validator: (value) {
-            if (value == null) return "Enter pickup point";
-            return null;
-          },
-        ),
+        ],)
       ),
     );
   }
