bf47b0eb012a66caf1f7664636423bd1a1d02606
Willian Dos Santos Alves <102175608+Celtavhc2012@users.noreply.github.com>
2022-05-31 17:21:13 -0300

    R02_T2_Envie_um_item

---
+++ uberclone/lib/views/SendAnItemConfirmed/SendAnItemConfirmed.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SendAnItemConfirmed extends StatelessWidget {
+  const SendAnItemConfirmed({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        leading: OutlinedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://futurainfo.net/wp-content/uploads/2018/12/Checklist-Graphic-Icon-300x300.png'),
+              width: 200,
+              height: 100,
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 8)),
+          const Text(
+            "Envio confirmado!",
+            style: TextStyle(
+                color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
