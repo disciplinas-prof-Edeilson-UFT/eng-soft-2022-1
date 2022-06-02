7ea18811fe5fe1546d398d7644d484af42f39e91
Armano Alves <69471768+armanoalves@users.noreply.github.com>
2022-06-02 18:15:15 -0300

    Develop (#80)
    * Pasta R01
    
    * T1 - [Front-end] Construir a tela da carteira do usuário @jhonatabatista1T1
    
    * T1 - [Front-end] Construir a tela da carteira do usuário @jhonatabatista1T1
    
    * R01-T2-Back-end
    
    * informação de Ponto-Destino para o firebase
    
    * informação de Ponto-Destino para o firebase
    
    * R02 T3 Receba Um Item
    
    * G03-R12 - Visualizar Última Viagem
    
    * G04-R15-Visualizar-configuracao-da-conta
    
    * T1 - [Front-end] Construir a tela de informações jurídicas @tatianeyumicolishibata1
    
    * R02_T3_Receba_Um_Item
    
    * R08-T1-Visualizar Home
    
    * R02_T3_Receba_Um_Item
    
    * R14-T1-Desenvolvimento da tela, T2-Envio de informações para o firebase
    
    * Pastas e arquivos adicionais para padronização
    
    * R02-T2-Envie um item
    
    * R02-T2-Envie um item ajustes
    
    * R02-T1-Front-End seleção de entrega
    
    * R02-T1-Front-End seleção de entrega Ajustes
    
    * R02_T3_Receba_Um_Item ajustado
    
    * R-03-visualizar-perfil-de-usuario final
    
    * R-03-visualizar-perfil-de-usuario ajustes
    
    * R13-T1-Visualizar-Carteira
    
    * R15-Visualizar Configuração da Conta
    
    * R09-solicitar ajuda
    
    * R32-Visualizar-Informações-Jurídicas
    
    * Update pubspec.yaml
    
    * Ajustes do aplicativos para lançar a release
    
    * G04-R18-Commit (#79)
    
    Ainda é preciso configurar a transição da tela.
    
    * Adicão de mais uma tela
    
    Co-authored-by: EysuuT <marcosdyeimison@gmail.com>
    Co-authored-by: JhonataBast <58173651+JhonataBast@users.noreply.github.com>
    Co-authored-by: Yuri Nascimento <yurinascimennto@gmail.com>
    Co-authored-by: Willian Dos Santos Alves <102175608+Celtavhc2012@users.noreply.github.com>
    Co-authored-by: Enzo Lucas <enzoolucass@gmail.com>
    Co-authored-by: Luccas Souza <emaildocastro.adm01@gmail.com>
    Co-authored-by: TaatiShibata <taati.shibata@gmail.com>
    Co-authored-by: maria julia <mjsfgn@outlook.com>
    Co-authored-by: Marcos Dyeimison Moreira da Silva <88058645+M-Dyeimison@users.noreply.github.com>
    Co-authored-by: ResqueJP <102630414+ResqueJP@users.noreply.github.com>
    Co-authored-by: lucasdias33 <92815322+lucasdias33@users.noreply.github.com>
    Co-authored-by: ITALO G MAIA <79777719+ItaloGbrmaia@users.noreply.github.com>
    Co-authored-by: italoGbrmaia <italogabriel.pm17@gmail.com>
    Co-authored-by: Yuri Nascimento <62949626+n0xcimento@users.noreply.github.com>
    Co-authored-by: Guilherme da Silva Rosario <50756442+GuiRosario@users.noreply.github.com>
    Co-authored-by: Luccas Castro <83096803+luccasocastro@users.noreply.github.com>
    Co-authored-by: Izabela Sena <isabelasena1297@gmail.com>
    Co-authored-by: Izabela Sena <84288848+I-sena@users.noreply.github.com>
    Co-authored-by: Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>

---
+++ uberclone/lib/views/CadastroMotorista/r18_cadastro_como_motorista_do_uber.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:uberclone/utilities/defaultColors.dart';
+import 'package:uberclone/views/CadastroMotorista/segundaTela.dart';
+
+class R18 extends StatefulWidget {
+  const R18({Key? key}) : super(key: key);
+
+  @override
+  State<R18> createState() => _R18State();
+}
+
+class _R18State extends State<R18> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundMain,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'Uber',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: ListView(
+            children: [
+              const Text(
+                'Inscreva-se para dirigir',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Uber Move Bold',
+                ),
+              ),
+              const SizedBox(height: 25.0),
+              const Text(
+                "•Conheça mais pessoas em sua cidade\n•Ganhe dinheiro extra dirigindo quando quiser\n•Ganhe dinheiro rápido",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                    fontFamily: 'Uber Move Medium'),
+              ),
+              const SizedBox(height: 30.0),
+              const Text(
+                'Escolha uma cidade',
+                style: TextStyle(
+                  fontFamily: 'Uber Move Medium',
+                  fontSize: 15.0,
+                  color: Colors.white,
+                ),
+              ),
+              const TextField(
+                //controller: _textController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 25.0),
+              Image.asset(
+                'assets/image/banner01.jpeg',
+              ),
+              const SizedBox(height: 10.0),
+              const Text(
+                  'Ao continuar, eu concordo que a Uber e seus representantes podem entrar em contato comigo por e mail, telefone ou SMS (inclusive por sistema de discagem telefônica automática) no endereço de e-mail ou número que eu forneci, inclusive para finalidades de marketing.\n\nPara participar de uma promoção, você deve ver a oferta e se cadastrar para dirigir com a Uber nesta página até 1º de maio de 2023. A promoção se aplica apenas aos ganhos totais de suas primeiras viagens com a Uber. Esse valor não inclui extras, outras promoções ou taxas pagas à Uber. Para participar, você deve se cadastrar na cidade estipulada e concluir o número mínimo de viagens até 90 dias depois do cadastro. A Uber cobrirá a diferença entre o valor mínimo promocional e seus ganhos totais caso eles fiquem abaixo do indicado na promoção. Viagens canceladas não contam como viagens concluídas. As ofertas de promoção e os requisitos mínimos de viagem podem variar conforme a cidade. Nós nos reserva mos o direito de reter ou debitar pagamentos caso haja prova ou suspeita de erro, fraude, ação ilegal ou violação dos termos de motorista parceiro ou destes termos. Promoção disponível por tempo limitado. Os termos estão sujeitos a alterações.',
+                  style: TextStyle(
+                    fontFamily: 'Uber Move Medium',
+                    fontSize: 15.0,
+                    color: Colors.white,
+                  )),
+              const SizedBox(height: 10.0),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: 400,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SegundaTela()),
+                      );
+                    },
+                    child: const Text(
+                      'Continuar',
+                      style: TextStyle(
+                        fontFamily: 'Uber Move Medium',
+                        fontSize: 25,
+                        color: Colors.black,
+                        //textColor: Colors.white,
+                        //elevation: 5,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
